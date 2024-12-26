8a00b329db719a89d4d10e92625629ee3ac33dfb
hendricwidjaja <hcwidjaja@gmail.com>
2024-12-26 14:14:02 +1100

    first commit: Import fonts

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="font-lacquer text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+};
+
+export default App;

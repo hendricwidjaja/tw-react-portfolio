98946a3388aa101854be607b9f2dbac719e311d5
hendricwidjaja <hcwidjaja@gmail.com>
2025-11-21 21:25:41 +1100

    fix: test previous CV

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { HERO_CONTENT } from "../constants";
 import profile from "../assets/profile-picture2.jpg";
-import cv from "../assets/HendricWidjaja_CSS_CV.pdf";
+import cv from "../assets/Hendric_Widjaja_CV.pdf";
 import { motion } from "framer-motion";
 
 import { FaLinkedinIn } from "react-icons/fa6";
